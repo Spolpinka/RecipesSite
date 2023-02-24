fce7f46ac72d4baa8bcf6ea87366dfc144aff3c9
Spolpinka <a421243266@gmail.com>
2023-02-24 12:14:49 +0300

    first commit

---
+++ src/main/java/pro/sky/recipessite/RecipesSiteApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.recipessite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/recipessite/RecipesSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.recipessite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
